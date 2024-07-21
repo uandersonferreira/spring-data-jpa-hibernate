06376bd9ae9026bba7fb99ed5bf36a2fe4fd6b9c
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-07-21 16:47:13 -0300

    Salvando uma entidade na base de dados com jpa/hibernate.

--- jpa-hibernate-01/src/main/java/br/com/uanderson/Main.java
+++ jpa-hibernate-01/src/main/java/br/com/uanderson/Main.java
@@ -1,17 +1,54 @@
 package br.com.uanderson;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import br.com.uanderson.entities.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Criar a fábrica de EntityManager (EntityManagerFactory)
+        try (EntityManagerFactory entityManagerFactory =
+                     Persistence.createEntityManagerFactory("my-persistence-unit")) {
+
+            /* EntityManager -> Persistence Context */
+            // Criar o EntityManager que gerencia o contexto de persistência
+            try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+
+                // Iniciar uma transação
+                entityManager.getTransaction().begin();
+
+                // Criar uma nova instância da entidade Student
+                Student student1 = new Student();
+
+                // Definir o ID do estudante
+                student1.setId(1L);
+
+                // Definir o nome do estudante
+                student1.setName("Uanderson Oliveira");
+
+                // Persistir/Passar a existir no contexto de persistẽncia pois queremos (salvar)
+                // a entidade student1 no banco de dados
+                entityManager.persist(student1);
+
+                // Confirmar a transação (salvar as alterações no banco de dados)
+                entityManager.getTransaction().commit();
+            }
+
         }
-    }
-}
+
+    }//main
+
+}//class
+
+/*
+Resumo do Fluxo
+1. Configuração: Configura o EntityManagerFactory com a unidade de persistência especificada no persistence.xml.
+2. Criação do EntityManager: Cria um EntityManager para gerenciar o contexto de persistência.
+3. Transação: Inicia uma transação.
+4. Manipulação da Entidade: Cria uma nova entidade Student, configura seus atributos e a persiste no banco de dados.
+5. Commit: Confirma a transação para salvar as mudanças no banco de dados.
+6. Fechamento: Fecha os recursos (EntityManager e EntityManagerFactory).
+ */
